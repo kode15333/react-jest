d915105347b295d6146bdfe96cadda30f04e6217
JunhoYou <kode15333@gmail.com>
2020-09-15 17:06:24 +0900

    how to use customHook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useChecks} from "./components/useChecks";
 
 function App() {
+           const [isAllChecked, renderChecks] = useChecks(labels)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        {renderChecks()}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button disabled={!isAllChecked}>다음</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
   );
 }
 
 export default App;
+
+const labels = ['check 1', 'check 2', 'check 3']
+
